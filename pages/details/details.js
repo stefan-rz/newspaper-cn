047510933d2e134cb7edbc41427f65a35166b5aa
Ruihan Zou <rhzou@amazon.com>
2018-04-18 00:06:47 +0800

    add tap events for new lists

--- pages/details/details.js
+++ pages/details/details.js
@@ -5,62 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    id:0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad(options) {
+    this.setData({
+      id: options.id
+    })
+    this.getNews()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  getNews() {
+    console.log(this.data.id)
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data: {
+        id: this.data.id
+      },
+      success: res => {
+        let result = res.data.result
+        let arr = result.content
+        this.setData({
+          id: result.id,
+          content: arr
+      })
+     }
+   })
   }
 })
